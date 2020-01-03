9164ae0b60eb20b4812d9d71ac6c63bfa1ce52c2
xXSchrandXx <marvinschrand@yahoo.de>
2020-01-03 22:00:01 +0100

    Remove useless

--- NetherPortalGate/docs/javadoc/0.0.5-SNAPSHOT/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"de.xxschrandxx.npg"},{"l":"de.xxschrandxx.npg.api"},{"l":"de.xxschrandxx.npg.api.config"},{"l":"de.xxschrandxx.npg.api.event"},{"l":"de.xxschrandxx.npg.command"},{"l":"de.xxschrandxx.npg.listener"}]
--- ServerStatusSign/docs/javadoc/0.0.4-SNAPSHOT/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"de.xxschrandxx.sss"},{"l":"de.xxschrandxx.sss.bukkit"},{"l":"de.xxschrandxx.sss.bukkit.api"},{"l":"de.xxschrandxx.sss.bukkit.command"},{"l":"de.xxschrandxx.sss.bukkit.listener"},{"l":"de.xxschrandxx.sss.bungee"},{"l":"de.xxschrandxx.sss.bungee.api"},{"l":"de.xxschrandxx.sss.bungee.listener"}]
