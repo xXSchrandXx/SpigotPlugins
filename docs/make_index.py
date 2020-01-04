abb5b962dc049087a007b81af5c601c8a5acb2f2
xXSchrandXx <marvinschrand@yahoo.de>
2020-01-04 12:26:30 +0100

    Update python.yml

--- docs/make_index.py
+++ docs/make_index.py
@@ -89,7 +89,7 @@ def fun(dir,rootdir):
     f.close()
     for subdir in dirnames:
         try:
-            if dir not like 'apidocs'
+            if dir not in 'apidocs'
               fun(dir+subdir+"/",rootdir+'../')
         except:
             pass
